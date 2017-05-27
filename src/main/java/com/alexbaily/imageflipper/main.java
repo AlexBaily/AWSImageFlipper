9ac10bfc5c16cdd24a4b841c0dfb054896d60a12
AlexBaily <alex.d.baily@gmail.com>
2017-05-27 17:54:19 +0100

    add handling of messages and outputting contents onto screen

--- src/main/java/com/alexbaily/imageflipper/main.java
+++ src/main/java/com/alexbaily/imageflipper/main.java
@@ -1,5 +1,7 @@
 package com.alexbaily.imageflipper;
 
+import java.util.List;
+
 import com.amazonaws.auth.AWSCredentialsProvider;
 import com.amazonaws.auth.profile.ProfileCredentialsProvider;
 import com.amazonaws.regions.Regions;
@@ -7,6 +9,9 @@ import com.amazonaws.services.sqs.AmazonSQS;
 import com.amazonaws.services.sqs.AmazonSQSClientBuilder;
 import com.amazonaws.services.sqs.model.AmazonSQSException;
 import com.amazonaws.services.sqs.model.CreateQueueRequest;
+import com.amazonaws.services.sqs.model.Message;
+import com.amazonaws.services.sqs.model.ReceiveMessageRequest;
+import com.amazonaws.services.sqs.model.ReceiveMessageResult;
 
 public class main {
 	
@@ -14,16 +19,31 @@ public class main {
 	
 	public static void main(String[] args) {
 		
-		
+		//Using a builder to return the queue, credentials are pulled from local machine.
 		AWSCredentialsProvider credentialsProvider = new ProfileCredentialsProvider();
 		AmazonSQS sqs = AmazonSQSClientBuilder.standard()
 											.withCredentials(credentialsProvider)
 											.withRegion(Regions.EU_WEST_1)
 											.build();
-		
+		//Debugging: Checking to make sure I have return the right queue.
 		String queue_url = sqs.getQueueUrl(QUEUE_NAME).getQueueUrl();
-		
 		System.out.println(queue_url);
+		
+		//Receive the messages via the ReceiveMessageResult class.
+		ReceiveMessageResult message = sqs.receiveMessage(queue_url);
+		//getMessages() returns a list of messages.
+		List<Message> messageList = message.getMessages();
+		
+		//for-each to loop through each message and see what they contain.
+		for(Message imageInfo : messageList) {
+			//Getting the contents of the message
+			String contents = imageInfo.getBody();
+			System.out.println(contents);
+			//Getting the message Handle for deletion.
+			String messageHandle =  imageInfo.getReceiptHandle();
+			sqs.deleteMessage(queue_url, messageHandle);
+
+		}
 		
 	}
 
